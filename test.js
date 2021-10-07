d7d890f088835204e31f9152adf300ca73952d7b
mntya <45312206+mntya@users.noreply.github.com>
2021-10-07 12:49:25 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-alert("hello world");
+// ==UserScript==
+// @name         Test1
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *
+// @icon         https://www.google.com/s2/favicons?domain=csdn.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.setTimeout(function() {
+        alert("hello world!");
+    }, 60);
+})();
