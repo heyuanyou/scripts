c09adca1180e94ec253dbab8d4c1caeb2312412d
mntya <45312206+mntya@users.noreply.github.com>
2021-12-10 20:58:21 +0800

    Create pstake.js

---
+++ pstake.js
@@ -0,0 +1,16 @@
+$("label[class='c-label c-label--inline s-marginLeft1']").each(function(){
+    var trim = $(this).text().replace(/\s/g,"");
+    if(trim=='25,000,000'.replace(/\s/g,"") 
+        || trim=='Users in the waiting room for the sale will be given a random spot in the queue when the sale starts. Users who arrive after the sale starts for the sale will be placed behind those in the waiting room'.replace(/\s/g,"") 
+        || trim=='Multichain'.replace(/\s/g,"")
+        || trim=='Liquid Staking'.replace(/\s/g,"")
+        || trim=='BTC, ETH, USDC, USDT,ALGO,SOL'.replace(/\s/g,"")
+        || trim=='$0.40 per token, $500 limit'.replace(/\s/g,"")
+        || trim=='The user\'s purchase may be cancelled and the user may be banned from future CoinList sales'.replace(/\s/g,"")
+       
+        || trim=='CoinList.co'.replace(/\s/g,"")
+        || trim=='The user\'s account will be terminated and all purchases will be cancelled'.replace(/\s/g,"")
+    ){
+        $(this).children('input').click()
+    }
+})
