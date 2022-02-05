e9507d1409db73adb2339b2c3c241c6b96943c9a
mntya <45312206+mntya@users.noreply.github.com>
2022-02-05 11:28:19 +0800

    Create nym-option-2.js

---
+++ nym-option-2.js
@@ -0,0 +1,17 @@
+$("label[class='c-label c-label--inline s-marginLeft1']").each(function(){
+    var trim = $(this).text().replace(/\s/g,"");
+    if(trim=='Option 1: 50,000; Option 2: 6,250'.replace(/\s/g,"") 
+        || trim=='Users in the waiting room will be given a random spot in the queue when the sale starts. Users who arrive after the sale starts will be placed behind those in the waiting room'.replace(/\s/g,"") 
+        || trim=='coinlistofficialchannel'.replace(/\s/g,"")
+        || trim=='Option 1: Tokens release on / around March 31, 2022. Option 2: Tokens release quarterly over two years starting on / around May 3, 2022'.replace(/\s/g,"")
+        || trim=='BTC, ETH, USDC, USDT, SOL, ALGO'.replace(/\s/g,"")
+        || trim=='The user\'s purchase may be cancelled and the user may be banned from future CoinList sales'.replace(/\s/g,"")
+        || trim=='CoinList.co'.replace(/\s/g,"")
+        || trim=='Option 1: $0.50 per token, $500 limit. Option 2: $0.25 per token, $1000 limit'.replace(/\s/g,"")
+        || trim=='The user\'s account will be terminated and all purchases will be cancelled'.replace(/\s/g,"")
+    ){
+        $(this).children('input').click()
+    }
+})
+$('html,body').animate({scrollTop:$('.s-marginTop2').offset().top}, 600);
+setTimeout(function(){ $('.c-button--large')[0].click() }, 5000); //设置延迟9秒自动提交，延迟时间可自己修改
